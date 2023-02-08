c0e7be3d7a660c332579a0355444b47ea47164a4
brotifypacha <brotifypacha@gmail.com>
2023-02-08 22:05:29 +0300

    Add go:generate command for grafana/repository interface

--- internal/grafana/repository.go
+++ internal/grafana/repository.go
@@ -2,6 +2,7 @@ package grafana
 
 import "github.com/brotifypacha/grafana_searcher/internal/domain"
 
+//go:generate mockgen -source repository.go -package grafana -destination mockRepository.go
 type Repository interface {
 	GetTree() (*domain.GrafanaFolder, error)
 	GetDashboard(uid string) ([]byte, error)
